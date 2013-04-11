9d763c74df88b69f7db6f51da842546e37c2ae27
Adolfo <mac.adolfo@gmail.com>
2013-04-11 11:25:33 +0100

    Initial Commit

---
+++ Calculator/src/ie/gcd/maths/Calc.java
@@ -0,0 +1,21 @@
+package ie.gcd.maths;
+
+public class Calc {
+
+	public Calc() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int add(int x,int y){
+		return x-y;
+	
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
